3a6c9f56a0aab296b09bf6e85bb993c563a1383f
Juan de Dios Marcos <marg.jd@gmail.com>
2016-01-25 16:35:29 -0600

    Tipos de Datos - Hola Mundo 2

---
+++ Basico/HolaMundo2/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hola Mundo 2");
+        //Byte ocupa un espacion de memoria de 1 byte  y soporta un rango de (128 a 127)
+        byte edad = 1;
+        // Short ocupa un espacio de memoria de 2 bytes y soporta un rango de (-32,768 a 32,767)
+        short edad1 =1;
+        //int Ocupa un espacio de memoria de 4 bytes y soporta un rango de (2,147,483,648 a 2,147,483,647)
+        int edad2=1;
+        // long ocupa un espacio de memoria de 8 bytes y soporta un rango de (-9,223,372,036,854,775,808  a 9,223,372,036,854,775,807)
+        long edad3 = 1;
+
+    }
+}
